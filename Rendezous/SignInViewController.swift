589e038c9c0ef6a7f92f09d2c85283f411e98f1c
Jason <jclu5293@gmail.com>
2016-05-14 11:59:50 -0700

    Implemented sign in

---
+++ Rendezous/SignInViewController.swift
@@ -0,0 +1,136 @@
+//
+//  SignInViewController.swift
+//  Rendezvous
+//
+//  Created by Jason Lu on 5/13/16.
+//  Copyright © 2016 Thorn Technologies. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInViewController: UIViewController {
+    
+    let ref = Firebase(url: "https://rend-ezvous.firebaseio.com")
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+
+    @IBAction func handleSignIn(sender: AnyObject) {
+        ref.authUser(emailField.text!, password: passwordField.text!, withCompletionBlock: { error, authData in
+        
+        if (self.emailField.text == "" || self.usernameField.text == "") && self.passwordField.text == ""
+        {
+            self.pleaseEnter()
+        }
+            
+        if error != nil
+        {
+            print("Unable to sign in user")
+            self.cannotSignIn()
+        }
+        else {
+            let uid = authData.uid
+            print("Login successful with uid: \(uid)")
+            
+            let user = ["username" : self.usernameField.text!]
+            self.ref.childByAppendingPath("USERS/\(uid)").setValue(user)
+        }
+        
+        })
+    }
+    
+    @IBAction func handleCreateAccount(sender: AnyObject) {
+        ref.createUser(emailField.text!, password: passwordField.text!, withValueCompletionBlock: { error, result in
+            if (self.emailField.text == "" || self.usernameField.text == "") && self.passwordField.text == ""
+            {
+                self.pleaseEnter()
+            }
+            
+            if error != nil
+            {
+                print("Account already exists")
+                self.cannotCreateAccount()
+            }
+            else
+            {
+                let uid = result["uid"] as? String
+                print("Successfully created user with uid: \(uid)")
+            }
+        })
+    }
+    
+    
+
+    
+    func cannotSignIn () {
+        let alert = UIAlertController (title: "Unable to Sign In", message: "Username/email and/or password incorrect", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let ok = UIAlertAction(title: "Ok",
+            style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
+                print("OK")
+        }
+        
+        alert.addAction(ok)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    func cannotCreateAccount () {
+        let alert = UIAlertController (title :"Unable to Create Account",
+                    message: "Username or email already exists",
+                    preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let ok = UIAlertAction(title: "Ok",
+                               style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
+                                print("OK")
+        }
+        
+        alert.addAction(ok)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func pleaseEnter () {
+        let alert = UIAlertController (title :"Unable to Sign In or Create Account",
+                                       message: "Please enter your email/username and password",
+                                       preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let ok = UIAlertAction(title: "Ok",
+                               style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
+                                print("OK")
+        }
+        
+        alert.addAction(ok)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
