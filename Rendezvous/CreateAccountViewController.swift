a8012a42d08251e6228ed069d36a398951c32dcd
Jason <jclu5293@gmail.com>
2016-05-28 19:59:47 -0700

    Fixed Keyboard Problems

--- Rendezvous/CreateAccountViewController.swift
+++ Rendezvous/CreateAccountViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     
     let ref = Firebase(url: "https://rend-ezvous.firebaseio.com")
     
@@ -44,6 +44,11 @@ class CreateAccountViewController: UIViewController {
                 let number = self.phoneNumberField.text!
                 let name = ["Username" : self.nameField.text!]
                 self.ref.childByAppendingPath("USERS/\(number)").setValue(name)
+                
+                
+                //let user = ["username" : self.usernameField.text!]
+                //self.ref.childByAppendingPath("USERS/\(uid)").setValue(name)
+                //self.ref.childByAppendingPath("USERS/\(uid)").setValue(number)
                 
                 self.performSegueWithIdentifier("segueToMap", sender: nil)
             }
@@ -83,8 +88,13 @@ class CreateAccountViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        nameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        phoneNumberField.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -92,15 +102,12 @@ class CreateAccountViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Dismiss the keyboard when the user taps the "Return" key or its equivalent
+    // while editing a text field.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
     }
-    */
-
+    
 }
+
--- Rendezvous/SignInViewController.swift
+++ Rendezvous/SignInViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextFieldDelegate {
     
     let ref = Firebase(url: "https://rend-ezvous.firebaseio.com")
     
@@ -39,63 +39,10 @@ class SignInViewController: UIViewController {
                 let uid = authData.uid
                 print("Login successful with uid: \(uid)")
                 
-                //let user = ["username" : self.usernameField.text!]
-                //self.ref.childByAppendingPath("USERS/\(uid)").setValue(user)
                 self.performSegueWithIdentifier("segueToMap", sender: nil)
             }
         })
     }
-    /*
-    @IBAction func handleSignIn(sender: AnyObject) {
-        ref.authUser(emailField.text!, password: passwordField.text!, withCompletionBlock: { error, authData in
-            
-        if self.emailField.text == "" && self.passwordField.text == ""
-        {
-            self.pleaseSignIn()
-        }
-            
-        if error != nil
-        {
-            print("Unable to sign in user")
-            self.cannotSignIn()
-            
-        }
-        else {
-            let uid = authData.uid
-            print("Login successful with uid: \(uid)")
-            
-            //let user = ["username" : self.usernameField.text!]
-            //self.ref.childByAppendingPath("USERS/\(uid)").setValue(user)
-            self.performSegueWithIdentifier("segueToMap", sender: nil)
-        }
-            
-        })
- 
-    }
-    */
-    /*
-    @IBAction func handleCreateAccount(sender: AnyObject) {
-        ref.createUser(emailField.text!, password: passwordField.text!, withValueCompletionBlock: { error, result in
-            
-            if self.emailField.text == "" && self.usernameField.text == "" && self.passwordField.text == ""
-            {
-                self.pleaseCreateAccount()
-            }
-            
-            if error != nil
-            {
-                print("Account already exists")
-                self.cannotCreateAccount()
-            }
-            else
-            {
-                let uid = result["uid"] as? String
-                print("Successfully created user with uid: \(uid)")
-                self.performSegueWithIdentifier("segueToMap", sender: nil)
-            }
-        })
-    }
-    */
     
 
     
@@ -112,23 +59,7 @@ class SignInViewController: UIViewController {
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    /*
-    func cannotCreateAccount () {
-        let alert = UIAlertController (title :"Unable to Create Account",
-                    message: "Email already exists",
-                    preferredStyle: UIAlertControllerStyle.Alert)
-        
-        let ok = UIAlertAction(title: "Ok",
-                               style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
-                                print("OK")
-        }
-        
-        alert.addAction(ok)
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    */
- 
+   
     func pleaseSignIn () {
         let alert = UIAlertController (title :"Unable to Sign In",
                                        message: "Enter your email and password",
@@ -144,28 +75,14 @@ class SignInViewController: UIViewController {
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    /*
-    func pleaseCreateAccount () {
-        let alert = UIAlertController (title :"Unable to Create Account",
-                                       message: "Enter your email, username and password",
-                                       preferredStyle: UIAlertControllerStyle.Alert)
-        
-        let ok = UIAlertAction(title: "Ok",
-                               style: UIAlertActionStyle.Default) { (action: UIAlertAction) -> Void in
-                                print("OK")
-        }
-        
-        alert.addAction(ok)
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    */
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -173,21 +90,13 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-}
-
-extension SignInViewController: UITextFieldDelegate {
+    // Dismiss the keyboard when the user taps the "Return" key or its equivalent
+    // while editing a text field.
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        view.endEditing(true)
-        return true
+        textField.resignFirstResponder()
+        return true;
     }
+
 }
+
+
